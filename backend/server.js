b6463ff7eebe37239101457f8b07024b82b1e1e6
Shivangi-128-bit <mishrashivangi3232@gmail.com>
2025-06-02 18:24:47 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -41,3 +41,6 @@ app.post('/api/contact', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+// node server.js -- to start the server
+// Ensure MongoDB is running before starting the server and you are in same directory as server.js file is
